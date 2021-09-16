8d8eb341fff85e5fc4bbcf98f93ff8ec90e4f528
progyan <yanb@protonmail.com>
2021-09-16 19:15:38 +0300

    bugfix

--- create_schema.sql
+++ create_schema.sql
@@ -1,9 +1,22 @@
 CREATE TABLE IF NOT EXISTS news
 (
-    id serial NOT NULL,
-    creator varchar(64) NOT NULL,
-    title varchar(512) NOT NULL,
-    content text NOT NULL,
-    news_type varchar(64) NOT NULL,
-    PRIMARY KEY (id)
-);
+    id integer NOT NULL DEFAULT nextval('news_id_seq'::regclass),
+    title character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    content character varying(8192) COLLATE pg_catalog."default" NOT NULL,
+    creator character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    news_type character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    creation_date date,
+    receiving_group integer[],
+    CONSTRAINT news_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    username character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    can_write_news boolean,
+    priority integer,
+    receiving_group integer[],
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
