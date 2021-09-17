3ef59ed01e247de97505f462bdebd7d2c3ae12f5
progyan <yanb@protonmail.com>
2021-09-17 12:49:53 +0300

    Bugfix

--- static/news/script.js
+++ static/news/script.js
@@ -6,7 +6,7 @@ let news = JSON.parse(localStorage.getItem("currentNews"));
 
 document.getElementById("title").innerText = news[1].toUpperCase();
 document.getElementById("description").innerText = news[3] + " новость от " + lowercase_names[news[0]];
-document.getElementById("text").innerText = marked(news[2]);
+document.getElementById("text").innerHTML = marked(news[2]);
 document.getElementById("creator-name").innerText = "-- " + news[0];
 document.getElementById("face").src = "../faces/" + news[0] + ".jpg";
 
