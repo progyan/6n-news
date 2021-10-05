480bcb155110089ab1422fb93f50d6c37481bea2
progyan <yanb@protonmail.com>
2021-10-05 15:04:54 +0300

    Switched to HTTPS

--- flask_app.py
+++ flask_app.py
@@ -152,4 +152,4 @@ def get_connection():
     return psycopg2.connect(database='6n', user='postgres', password='postgres', 
             host='localhost', port=5432)
 
-#app.run()
+app.run(ssl_context='adhoc')
